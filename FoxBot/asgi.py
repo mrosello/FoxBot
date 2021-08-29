09691bffe12bdff419476defb45fd458aee79108
Runfox <maximerosello@gmail.com>
2021-08-29 09:13:20 +0200

    Changing FoxBot to foxbot for more consistancy

--- FoxBot/asgi.py
+++ FoxBot/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FoxBot project.
+ASGI config for foxbot project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FoxBot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foxbot.settings')
 
 application = get_asgi_application()
