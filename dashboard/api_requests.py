4880a4c779af84fc7b71c522ef5f66c6b6524fde
Runfox <maximerosello@gmail.com>
2021-08-30 17:04:07 +0200

    First API requests: client wallet
    Adding API request, corresponding view.py function & html rendering to customer waller

--- dashboard/api_requests.py
+++ dashboard/api_requests.py
@@ -6,11 +6,18 @@ import config
 def check_balance():
     """ found balances available for the client """
     try:
-        balance = Client(config.API_KEY, config.API_SECRET).get_account()['balances']
+        balance = Client(config.API_KEY, config.API_SECRET).get_account(recvWindow=59000)['balances']
     except ReadTimeout:
         balance = "ReadTimeout during check balance"
     except ConnectionError:
         balance = "ConnectionError during check balance"
-    except BinanceAPIException:
+    except BinanceAPIException as e:
+        print(e.status_code)
+        print(e.message)
         balance = "BinanceAPIException during check balance"
     return balance
+
+def check_all_prices():
+    """ for conversion purpose, convert btc to usd"""
+    prices = Client(config.API_KEY, config.API_SECRET).get_all_tickers()
+    return prices
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from dashboard.api_requests import (check_balance)
+from dashboard.api_requests import (check_balance, check_all_prices)
 
 def index(request):
     my_balance = check_balance()
-    data={'my_balance':my_balance}
+    all_prices = check_all_prices()
+    data={'my_balance':my_balance, 'all_prices':all_prices}
     return render(request, 'dashboard/index.html', context=data)
